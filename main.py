a97fb15f05bfd68b2c748f9d3519c63bee82f083
hiimtmac <taylor@hiimtmac.com>
2020-03-29 22:53:30 +0000

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+from rfidlib import RFID
+import time
+rdr = RFID()
+
+print("Starting...")
+
+while True:
+  rdr.wait_for_tag()
+  (error, tag_type) = rdr.request()
+  if not error:
+    print("Tag detected")
+    (error, uid) = rdr.anticoll()
+    if not error:
+      print("UID: " + str(uid))
+      time.sleep(5)
+
+# Calls GPIO cleanup
+rdr.cleanup()
